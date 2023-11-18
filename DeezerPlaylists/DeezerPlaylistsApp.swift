4cebab6368b1d42570d2ccf8f39d5a385f5c28b0
christopher james <chriisjms@gmail.com>
2023-11-18 02:19:25 +0100

    initial commit

---
+++ DeezerPlaylists/DeezerPlaylistsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeezerPlaylistsApp.swift
+//  DeezerPlaylists
+//
+//  Created by Christopher James on 18/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct DeezerPlaylistsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
